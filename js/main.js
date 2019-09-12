0ef5f6fc330ff5428b4c291cd2e7b19facad3f5a
iftekhar-rahman <iftekharmail1@gmail.com>
2019-09-12 05:29:40 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,118 @@
+jQuery(document).ready(function($){
+
+    // $('select').niceSelect();
+
+    // homepage-slides
+	// $(".homepage-slides").owlCarousel({
+	// 	items: 1,
+	// 	loop: true,
+	// 	nav: true,
+	// 	dots: false,
+    //     autoplay: false,
+    //     navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"]
+	// });
+
+    
+
+    // $("#range_49").ionRangeSlider({
+    //     type: "single",
+    //     min: 150,
+    //     max: 250,
+    //     from: 200,
+    //     to: 250,
+    //     prefix: "£",
+    //     step: 1,
+    //     onFinish: function(data) {
+    //         maxValue = data.from;
+    //         alert("Your Value: " + maxValue);
+    //     }
+    // });
+
+
+    // $(".logo-carousel-wrap").owlCarousel({
+    //     items: 5,
+    //     loop: true,
+    //     nav: false,
+    //     dots: false,
+    //     autoplay: false,
+    //     margin: 15,
+    //     navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+    //     responsive: {
+    //         0: {
+    //             items: 2,
+    //             margin: 15,
+    //             nav: false
+    //         },
+    //         580: {
+    //             items: 3,
+    //             margin: 3,
+    //             nav: false
+    //         },
+    //         720: {
+    //             items: 4,
+    //             margin: 3,
+    //             nav: false
+    //         },
+    //         960: {
+    //             items: 5
+    //         },
+    //         1140: {
+    //             items: 6
+    //         }
+
+    //     }
+    // });
+
+    // $(".feature-carousel-wrap").owlCarousel({
+    //     items: 4,
+    //     loop: true,
+    //     nav: true,
+    //     dots: true,
+    //     autoplay: false,
+    //     margin: 15,
+    //     navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+    //     responsive: {
+    //         0: {
+    //             items: 1,
+    //             margin: 0,
+    //             nav: false
+    //         },
+    //         481: {
+    //             items: 2,
+    //             margin: 15,
+    //             nav: false
+    //         },
+    //         720: {
+    //             items: 3,
+    //             margin: 15,
+    //             nav: false
+    //         },
+    //         960: {
+    //             items: 3,
+    //             margin: 25,
+    //         },
+    //         1140: {
+    //             items: 4
+    //         }
+
+    //     }
+    // });
+
+
+
+
+	// $(".bodytype-carousel-wrap").magnificPopup({
+	// 	type: 'image',
+    //     gallery: {
+    //         enabled: true
+    //     }
+	// });
+
+
+
+
+    
+
+    
+
+});
